3d94de843fe16b3b94f26a9a0f0706787ffddeed
Ignacio Medina Fernandez <111135931+NacMed94@users.noreply.github.com>
2023-01-30 12:40:59 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='genutils',
+      version='0.1',
+      description='What the module does',
+      url='https://github.com/NacMed94/pycho',
+      author='Ignacio Medina Fernandez',
+      author_email='nacmed94@gmail.com',
+      license='GNU v3',
+      packages=['genutils']
